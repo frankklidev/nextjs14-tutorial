a013a3254832d5a0f818f179b06ae82816745668
Franklin <frankkli.personal@gmail.com>
2024-02-04 13:32:53 -0500

    first commit

---
+++ app/ui/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerPage = () => {
+  return (
+    <h1>Este es el CustomerPage</h1>
+  )
+}
+
+export default CustomerPage
---
+++ app/ui/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoicePage = () => {
+  return (
+   <h1>Este es el invoice</h1>
+  )
+}
+
+export default InvoicePage
